905bec89b4bec88c57b1fe50e61b104287ef6b8b
Paul Mietz Egli <paul@obscure.com>
2011-05-05 11:42:49 -0700

    adding assets classes, though I thought they were generated...

---
+++ Classes/ComObscureCouchbase_tiModuleAssets.m
@@ -0,0 +1,15 @@
+/**
+ * This is a generated file. Do not edit or your changes will be lost
+ */
+#import "ComObscureCouchbase_tiModuleAssets.h"
+
+extern NSData * dataWithHexString (NSString * hexString);
+
+@implementation ComObscureCouchbase_tiModuleAssets
+
+- (NSData*) moduleAsset
+{
+	return nil;
+}
+
+@end

9faa7ba0ae2437d258acb9067bcf86f99a54dffa
Paul Mietz Egli <paul@obscure.com>
2012-01-20 15:23:33 -0800

    added Android module support

--- samples/CouchBooks/Resources/app.js
+++ samples/CouchBooks/Resources/app.js
@@ -1,7 +1,7 @@
 var NavigationController = require('lib/NavigationController').NavigationController,
     StateMachine = require('lib/state-machine').StateMachine,
     CouchClient = require('lib/couch_client').CouchClient,
-    CouchServer = require('com.obscure.couchbase_ti'),
+    CouchServer = require('com.obscure.CouchbaseTi'),
     books = require('ui/books');
     
 var controller = new NavigationController();
@@ -74,7 +74,8 @@ var fsm = StateMachine.create({
       }
       else {
         var contents = f.read();
-        if (contents.mimeType !== 'text/json' && contents.mimeType !== 'application/json') {
+        // Android MIME type is application/octet-stream, so skip check on that platform
+        if (Ti.Platform.osname !== 'android' && contents.mimeType !== 'text/json' && contents.mimeType !== 'application/json') {
           self.show_error('Error reading seed data: wrong content type '+contents.mimeType);
           return;
         }
